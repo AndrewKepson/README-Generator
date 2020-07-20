1125c573dbacdeb52e9317d7d9864845fa55b363
Boba Fett <60700164+AndrewKepson@users.noreply.github.com>
2020-07-20 15:39:47 -0600

    Added badge and titles.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const questions = [
 ];
 
 const writeToFile = (fileName, data) => {
-    fs.appendFileSync(fileName, data, (error) => {
+    fs.writeFile(fileName, data, (error) => {
         if(error) console.log(error);
     });
 }
@@ -22,7 +22,24 @@ const writeToFile = (fileName, data) => {
 inquirer.prompt(questions)
     .then((answers) => {
         const answerValues = Object.values(answers);
-
-        answerValues.forEach((answer) => writeToFile('./Generated-README.md', `${answer}\n\n`))
+        writeToFile('./Generated-README.md',createReadMe(answerValues));
+        // answerValues.forEach((answer) => writeToFile('./Generated-README.md', `${answer}\n\n`))
     })
     .catch((error) => console.log(error));
+
+const createReadMe = (answerValues) => {
+    return `${renderBadge(answerValues[5])}\n\n
+#Title \n\n ${answerValues[0]}
+##Description \n\n ${answerValues[1]}
+##Table of Contents \n\n ${answerValues[2]}
+##Installation \n\n ${answerValues[3]}
+##Usage \n\n ${answerValues[4]}
+##License \n\n ${answerValues[5]}
+##Contributors \n\n ${answerValues[6]}
+##Tests \n\n ${answerValues[7]}
+##Questions \n\n ${answerValues[8]} `
+}
+
+const renderBadge = (license) => {
+    return `![GitHub License](https://img.shields.io/badge/license-${license}-blue.svg)`
+}
